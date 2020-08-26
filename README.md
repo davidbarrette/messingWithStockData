abdae97ac66091e692e371334f3b1c5726e40ebb
dbarrett <dbarrett@macalester.edu>
2020-08-26 00:08:14 -0500

    Can now pull a single stocks data and saved as a pandas dataframe

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # messingWithStockData
 Pull stock data and manipulate it 
+
+
+# Setup
+Following: https://www.youtube.com/watch?v=d2kXmWzfS0w
+
+pip install pandas
+pip install alpha_vantage
+
+Go to https://www.alphavantage.co/support/#api-key to get your API key
--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("hello")
+import pandas as pd 
+from alpha_vantage.timeseries import TimeSeries
+
+API_KEY = '4OBRDZAAXWHRRJGV'
+
+ts = TimeSeries(key=API_KEY, output_format='pandas')
+data, meta_data = ts.get_daily(symbol='MSFT', outputsize='full')
+print(data)
